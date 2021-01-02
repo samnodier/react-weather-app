c79d479501f0c9b393945167a45b5369e693cb8d
proprgrmmr <61632722+sam0132nodier@users.noreply.github.com>
2021-01-02 07:34:08 +0200

    Working on use effect hook

--- src/App.js
+++ src/App.js
@@ -14,45 +14,25 @@ function App() {
 		const API_KEY = '0fa0eb1029b422ba434ffff1758bff25';
 		const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
 		setURL(url);
-		// // const icon = `"http://openweathermap.org/img/wn/{data['weather'][0]['icon']}@2x.png")`
-		// fetch(url)
-		// 	.then(res => res.json())
-		// 	.then(
-		// 		(result) => {
-		// 			setIsLoaded(true);
-		// 			setData(result);
-		// 		},
-		// 		// Catch erros
-		// 		(error) => {
-		// 			setIsLoaded(true);
-		// 			setError(error);
-		// 		}
-		// 	)
-
-		// if (error) {
-		// 	console.log(`Error: ${error.message}`);
-		// } else if (!isLoaded) {
-		// 	console.log("Loading...");
-		// }
-		// console.log(data);
+		const icon = `"http://openweathermap.org/img/wn/{data['weather'][0]['icon']}@2x.png")`
 	}
 
-	// useEffect(() => {
-	// 	handleSubmit();
-	// });
+	useEffect(() => {
+		handleSubmit();
+	});
 	return (
 		<div className="App">
 			<div id="background">
 			  <img src={background} width="100%" height="100%" />
 			</div>
 			<div className="App__window">
-				{
+				{/* {
 					if(isLoaded) {
 						<Details temperature={`${data["main"]["temp"]}°`} city={`${data["name"]}, ${data["sys"]["country"]}`} time_date="06:09 - Monday, 9 Sep 2020" icon={`http://openweathermap.org/img/wn/${data['weather'][0]['icon']}@2x.png`} status={data["weather"][0]["main"]} />
 						<Search handleSubmit={handleSubmit} longitudes={data["coord"]["lon"]} latitudes={data["coord"]["lat"]} temp_range={`${data["main"]["temp_min"]} - ${data["main"]["temp_max"]}`} humidity={`${data["main"]["humidity"]}%`} wind_speed={`${data["wind"]["speed"]}m/s`} cloudy={`${data["clouds"]["all"]}%`} />
 
 					}
-				}
+				} */}
 			</div>
 		</div>
 	);
